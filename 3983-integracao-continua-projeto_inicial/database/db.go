880a7bf43df196d9ab45c31d588503d270ec72ab
estefanirsouza <estefanirsouza@gmail.com>
2026-02-13 17:03:50 -0300

    Correcao

--- 3983-integracao-continua-projeto_inicial/database/db.go
+++ 3983-integracao-continua-projeto_inicial/database/db.go
@@ -2,23 +2,29 @@ package database
 
 import (
 	"log"
-
+	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var (
-	DB  *gorm.DB
-	err error
-)
+var DB *gorm.DB
+
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
+	var err error
+	endereco := os.Getenv("DB_HOST")
+	usuario := os.Getenv("DB_USER")
+	senha := os.Getenv("DB_PASSWORD")
+	nomeBanco := os.Getenv("DB_NAME")
+	portaBanco := os.Getenv("DB_PORT")
+	stringDeConexao := "host=" + endereco + " user=" + usuario + " password=" + senha + " dbname=" + nomeBanco + " port=" + portaBanco + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
 	}
 
-	DB.AutoMigrate(&models.Aluno{})
+if err := DB.AutoMigrate(&models.Aluno{}); err != nil {
+    log.Fatal(err)
+}
 }
