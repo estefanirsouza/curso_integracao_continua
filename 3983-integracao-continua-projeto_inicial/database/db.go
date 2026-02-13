b323fd45847bfc017645d3af3679e23fc26c2b96
estefanirsouza <estefanirsouza@gmail.com>
2026-02-13 18:28:20 -0300

    Teste

--- 3983-integracao-continua-projeto_inicial/database/db.go
+++ 3983-integracao-continua-projeto_inicial/database/db.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
+
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,21 +12,45 @@ import (
 
 var DB *gorm.DB
 
+// função auxiliar para pegar variável de ambiente com valor padrão
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
 
 func ConectaComBancoDeDados() {
 	var err error
-	endereco := os.Getenv("DB_HOST")
-	usuario := os.Getenv("DB_USER")
-	senha := os.Getenv("DB_PASSWORD")
-	nomeBanco := os.Getenv("DB_NAME")
-	portaBanco := os.Getenv("DB_PORT")
-	stringDeConexao := "host=" + endereco + " user=" + usuario + " password=" + senha + " dbname=" + nomeBanco + " port=" + portaBanco + " sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+
+	// pega variáveis com fallback
+	endereco := getEnv("DB_HOST", "localhost")
+	usuario := getEnv("DB_USER", "root")
+	senha := getEnv("DB_PASSWORD", "root")
+	nomeBanco := getEnv("DB_NAME", "root")
+	portaBanco := getEnv("DB_PORT", "5432")
+
+	stringDeConexao := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		endereco,
+		usuario,
+		senha,
+		nomeBanco,
+		portaBanco,
+	)
+
+	fmt.Println("Conectando ao banco em:", endereco)
+
+	DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Panic("Erro ao conectar com banco de dados: ", err)
 	}
 
-if err := DB.AutoMigrate(&models.Aluno{}); err != nil {
-    log.Fatal(err)
-}
+	// AutoMigrate
+	if err := DB.AutoMigrate(&models.Aluno{}); err != nil {
+		log.Fatal("Erro ao fazer migration: ", err)
+	}
+
+	fmt.Println("Banco conectado com sucesso 🚀")
 }
